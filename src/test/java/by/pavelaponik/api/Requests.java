6455c5dab54cbf2337e3cb191075f67e0f546c6a
Pavel Aponik <aponikpavel@gmail.com>
2023-01-14 18:37:18 +0300

    refactor

---
+++ src/test/java/by/pavelaponik/api/Requests.java
@@ -0,0 +1,13 @@
+package by.pavelaponik.api;
+import io.restassured.path.json.JsonPath;
+import static io.restassured.RestAssured.given;
+
+public class Requests {
+
+    public static JsonPath sendGetRequestForSpecificCountry(String endPoint, Object country) {
+
+        JsonPath response = given().queryParams("codes", country).when().get(endPoint).then().extract().response().jsonPath();
+        return response;
+
+    }
+}
